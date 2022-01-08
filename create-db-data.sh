31034a5249795b35e8dafa06f6c13a0cc54ba21e
szabos <steve.g.szabo@gmail.com>
2022-01-08 17:49:04 +0000

    updates

--- create-db-data.sh
+++ create-db-data.sh
@@ -4,7 +4,13 @@ set -o errexit
 
 PROFILE=/vault/secrets/config
 
-[ -f $PROFILE ] && source $PROFILE
+if [ -f $PROFILE ]; then
+    source $PROFILE
+else
+    POSTGRES_USER=user
+    POSTGRES_PASSWORD=pass
+    export POSTGRES_USER POSTGRES_PASSWORD
+fi
 
 psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER <<-EOF
     CREATE DATABASE posts;
